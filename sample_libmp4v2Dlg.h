ba55d07afce2e5d86c6559694c0014d454218e6e
18059104@qq.com <huijia264>
2018-03-16 13:16:17 +0800

    完成工程初始化

---
+++ sample_libmp4v2Dlg.h
@@ -0,0 +1,32 @@
+
+// sample_libmp4v2Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// Csample_libmp4v2Dlg 对话框
+class Csample_libmp4v2Dlg : public CDialogEx
+{
+// 构造
+public:
+	Csample_libmp4v2Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_SAMPLE_LIBMP4V2_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
